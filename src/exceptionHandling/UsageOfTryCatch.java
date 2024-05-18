6991550559aa5337eca54eaeb1e6faf1846cd408
Pratibha <pratipw23@gmail.com>
2024-05-18 21:08:29 +0530

    Java-Concepts: Example of Try-Catch block in exception handling

---
+++ src/exceptionHandling/UsageOfTryCatch.java
@@ -0,0 +1,15 @@
+package exceptionHandling;
+
+public class UsageOfTryCatch {
+
+	public static void main(String[] args) {
+		try {
+			//code that will generate exception
+			int devideByZero=10/0;
+			System.out.println("Rest of the code");
+		}catch(ArithmeticException e) {
+			System.out.println("ArithmaticException->"+e.getMessage());
+		}
+	}
+
+}
